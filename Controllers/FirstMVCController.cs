f28c072aad3b52920cfe99f35812137da68359a5
mm13609 <mustafamohammad92@gmail.com>
2017-04-29 16:30:55 -0500

    branch test
    branch test

--- Controllers/FirstMVCController.cs
+++ Controllers/FirstMVCController.cs
@@ -16,7 +16,7 @@ namespace MvcApp.Controllers
 
         public void DoSomeThing()
         {
-            Response.Write("<script>alert('DoSomeThing...okay')</script>");
+            Response.Write("<script>alert('DoSomeThing...okay...branch test')</script>");
         }
     }
 }
